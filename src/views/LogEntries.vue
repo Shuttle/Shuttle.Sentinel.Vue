38574c66a519ea23bd9b2ba5521fc8a93b318dcc
Eben <me@ebenroux.co.za>
2022-10-16 09:54:19 +0200

    - log entries

--- src/views/LogEntries.vue
+++ src/views/LogEntries.vue
@@ -2,7 +2,9 @@
     <div>
         <Title>{{ $t("log-entries") }}</Title>
         <Strip>
-            <Button :icon="RefreshIcon" size="sm" @click="refresh"></Button>
+            <Input v-model="specification.categoryMatch" :placeholder="$t('category')"></Input>
+            <Listbox v-model="specification.maximumRows" :options="maximumRows" class="w-20" />
+            <Button :icon="SearchIcon" size="sm" @click="refresh"></Button>
         </Strip>
         <Table :fields="fields" :items="items" :busy="busy" striped>
             <template #item(endpoint)="data">
@@ -20,13 +22,25 @@
 
 <script setup>
 import api from "@/api";
-import { onMounted, ref } from "vue";
+import { onMounted, reactive, ref } from "vue";
 import { useI18n } from "vue-i18n";
-import { RefreshIcon } from "@heroicons/vue/outline";
+import { SearchIcon } from "@heroicons/vue/outline";
 import { useSecureTableFields } from "@/composables/useSecureTableFields";
 
 const { t } = useI18n({ useScope: 'global' });
 const busy = ref();
+const specification = reactive({
+    categoryMatch: "",
+    maximumRows: 100
+});
+
+const maximumRows = [
+    { text: '100', value: 100 },
+    { text: '500', value: 500 },
+    { text: '2000', value: 2000 },
+    { text: '5000', value: 5000 },
+    { text: '10000', value: 10000 },
+]
 
 const fields = useSecureTableFields([
     {
@@ -64,7 +78,7 @@ const refresh = () => {
     busy.value = true;
 
     api
-        .post("logentries/search", {})
+        .post("logentries/search", specification)
         .then(function (response) {
             if (!response || !response.data) {
                 return;
